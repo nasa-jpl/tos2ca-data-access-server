047325f2de5f8234199fcfdb455199c421eef785
Flynn Platt <flynn.platt@jpl.nasa.gov>
2024-05-22 11:01:43 -0700

    multi-thread file reads and add lat/lon data

--- src/data_util.py
+++ src/data_util.py
@@ -3,6 +3,7 @@ import json
 import time as pytime
 from json import JSONEncoder
 from collections import OrderedDict
+from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 from netCDF4 import Dataset, default_fillvals
 import logging
@@ -28,74 +29,47 @@ def walktree(top):
         yield from walktree(value)
 
 
-def get_plot_data(file_list=[], remove_fill=False):
-    logging.info(f"Collecting plot data for: {file_list}")
-    p_start_time = pytime.time()
+def get_data_from_file(filename):
+    varset = OrderedDict()
 
-    if not isinstance(file_list, list):
-        file_list = [file_list]
+    if os.path.isfile(filename):
+        nc = Dataset(filename)
+        nc.set_auto_mask(False)
+        for children in walktree(nc):
+            for child in children:
+                if len(child.groups) == 0 and child.name != "navigation":
+                    for var_name in child.variables:
+                        variable = child.variables[var_name]
 
-    fill_value = None
-    varset = OrderedDict()  # use OrderedDict to preserve input file list order
-    for filename in file_list:
-        if os.path.isfile(filename):
-            nc = Dataset(filename)
-            nc.set_auto_mask(False)
-            for children in walktree(nc):
-                for child in children:
-                    if len(child.groups) == 0 and child.name != "navigation":
-                        for var_name in child.variables:
-                            variable = child.variables[var_name]
+                        # collect time and phenomenon id into arrays
+                        (time, id) = child.path.strip("/").split("/")
+                        time_arr = np.full(len(variable), int(time), dtype=int)
+                        id_arr = np.full(len(variable), int(id), dtype=int)
 
-                            # init variable value tracking
-                            if var_name not in varset:
-                                varset[var_name] = {
-                                    "values": np.array([]),
-                                    "times": np.array([]),
-                                    "phenom_ids": np.array([]),
-                                }
+                        # init accumulator
+                        if var_name not in varset:
+                            units = variable.Units
 
-                            # add fill value and units string
-                            if (
-                                "fill_val" not in varset[var_name]
-                                or "units" not in varset[var_name]
-                            ):
-                                try:
-                                    # get fill value from defaults because it doesn't respect the metadata
-                                    dtype = variable.dtype.str.replace("<", "").replace(
-                                        ">", ""
-                                    )  # ignore byteorder char
-                                    fv = default_fillvals[dtype]
-                                    varset[var_name]["fill_val"] = fv
+                            # get fill value from defaults because it doesn't respect the metadata
+                            dtype = variable.dtype.str.replace("<", "").replace(
+                                ">", ""
+                            )  # ignore byteorder char
+                            fv = default_fillvals[dtype]
 
-                                    units = variable.Units
-                                    varset[var_name]["units"] = units
-
-                                    # check if this fill value is the same as the others
-                                    if fill_value == None:
-                                        fill_value = fv
-                                    elif fill_value != fv:
-                                        logging.warn(
-                                            f"WARNING: mismatched fill values f{fill_value}, {fv}"
-                                        )
-                                        remove_fill = False
-
-                                except AttributeError:
-                                    continue
-
-                            # collect measurement values
-                            new_vals = variable[:, 2]
+                            varset[var_name] = {
+                                "values": variable,
+                                "times": time_arr,
+                                "phenom_ids": id_arr,
+                                "fill_val": fv,
+                                "units": units,
+                            }
+                        else:
                             varset[var_name]["values"] = np.concatenate(
                                 (
                                     varset[var_name]["values"],
-                                    new_vals,
+                                    variable,
                                 )
                             )
-
-                            # collect time and phenomenon id
-                            (time, id) = child.path.strip("/").split("/")
-                            time_arr = np.full(new_vals.shape, int(time), dtype=int)
-                            id_arr = np.full(new_vals.shape, int(id), dtype=int)
                             varset[var_name]["times"] = np.concatenate(
                                 (
                                     varset[var_name]["times"],
@@ -108,33 +82,62 @@ def get_plot_data(file_list=[], remove_fill=False):
                                     id_arr,
                                 )
                             )
+    else:
+        logging.error(f"{filename} is not a file")
 
-        else:
-            print(f"{filename} is not a file")
+    return varset
+
+
+def get_plot_data(file_list=[], remove_fill=False):
+    logging.info(f"Collecting plot data for: {file_list}")
+    p_start_time = pytime.time()
+
+    if not isinstance(file_list, list):
+        file_list = [file_list]
+
+    # multi-thread the data collection
+    datasets = []
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for i in executor.map(get_data_from_file, file_list):
+            datasets.append(i)
+
+    # collapse all the file contents into a single dict for ease
+    varset = OrderedDict()
+    for dataset in datasets:
+        for varname in dataset:
+            varset[varname] = dataset[varname]
 
     # stack all the variable values together into a single nD array
     plotset = {}
     for var_name in varset:
+        vals = varset[var_name]["values"][:, 2]  # grab the value column
         if "values" not in plotset:
             plotset["title"] = to_title(var_name)
-            plotset["values"] = varset[var_name]["values"]
+            plotset["values"] = vals
             plotset["axis_labels"] = [f'{var_name} ({varset[var_name]["units"]})']
         else:
             plotset["title"] = f'{plotset["title"]} x {to_title(var_name)}'
-            plotset["values"] = np.c_[plotset["values"], varset[var_name]["values"]]
+            plotset["values"] = np.c_[plotset["values"], vals]
             plotset["axis_labels"].append(f'{var_name} ({varset[var_name]["units"]})')
 
-    # stack in the time and phenom ids
+    # stack in the lat, lon, time, and phenom ids and pull fill value
+    fill_value = None
     for var_name in varset:
+        lat_lon = varset[var_name]["values"][:, :2]  # grab the first two columns
         plotset["values"] = np.c_[
-            plotset["values"], varset[var_name]["times"], varset[var_name]["phenom_ids"]
+            plotset["values"],
+            lat_lon,
+            varset[var_name]["times"],
+            varset[var_name]["phenom_ids"],
         ]
-        break  # all the times and IDs should match so we just need to do this once
+        fill_value = varset[var_name]["fill_val"]
+        break  # all the lats, lons, times, IDs, and fill values should match so we just need to do this once
 
     # optionally remove rows that contain the fill value
-    # TODO - figure out why there are apparently multiple fill values?
     if remove_fill:
-        expected_fill = -9999.0
+        expected_fill = (
+            -9999.0
+        )  # TODO - figure out why there are apparently multiple fill values?
         mask = ~(
             np.logical_or(
                 plotset["values"] == fill_value, plotset["values"] == expected_fill
