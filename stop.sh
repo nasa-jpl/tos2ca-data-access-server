526b2d77d8597c7add17efe6511c4f05061416b0
Flynn Platt <flynn.platt@jpl.nasa.gov>
2024-08-21 15:29:44 -0700

    add stop script

---
+++ stop.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+echo "sourcing .env"
+set -a
+source .env
+set +a
+
+echo "stopping containers"
+docker stop data_access_server_app
+docker stop data_access_server_proxy
+
+echo "pausing..."
+sleep 3
+
+echo "removing containers"
+docker rm data_access_server_app
+docker rm data_access_server_proxy
+
+echo "pausing..."
+sleep 3
+
+echo "removing network"
+docker network rm app_services_network
