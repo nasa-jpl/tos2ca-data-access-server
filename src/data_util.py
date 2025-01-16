2daf6f2daa7458dabb2db29f1045e85825ab6006
Flynn Platt <Flynn.Platt@jpl.nasa.gov>
2025-01-15 16:38:18 -0800

    fix object construction

--- src/data_util.py
+++ src/data_util.py
@@ -148,7 +148,7 @@ def get_plot_data(
         # for varname in vset:
             # varset[varname] = vset[varname]
         for varname in sset:
-            varset[varname]["stats"] = sset[varname]
+            varset[varname] = {"stats": sset[varname]}
             var_times = list(sset[varname].keys())
             time_arr.extend(var_times)
             for var_time in var_times:
