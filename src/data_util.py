31c82eddd1cb873d675bb0113c1f4e67e8a41976
Flynn Platt <Flynn.Platt@jpl.nasa.gov>
2025-01-15 16:24:50 -0800

    try removing all the individual data point reading

--- src/data_util.py
+++ src/data_util.py
@@ -42,45 +42,45 @@ def get_data_from_file(filename, times=None):
                         if times and (time < times[0] or time > times[1]):
                             continue
 
-                        time_arr = np.full(len(variable), time, dtype=int)
-                        id_arr = np.full(len(variable), id, dtype=int)
+                        # time_arr = np.full(len(variable), time, dtype=int)
+                        # id_arr = np.full(len(variable), id, dtype=int)
 
                         # init accumulator
-                        if var_name not in varset:
-                            units = variable.Units
+                        # if var_name not in varset:
+                        #     units = variable.Units
 
-                            # get fill value from defaults because it doesn't respect the metadata
-                            dtype = variable.dtype.str.replace("<", "").replace(
-                                ">", ""
-                            )  # ignore byteorder char
-                            fv = default_fillvals[dtype]
+                        #     # get fill value from defaults because it doesn't respect the metadata
+                        #     dtype = variable.dtype.str.replace("<", "").replace(
+                        #         ">", ""
+                        #     )  # ignore byteorder char
+                        #     fv = default_fillvals[dtype]
 
-                            varset[var_name] = {
-                                "values": variable,
-                                "times": time_arr,
-                                "phenom_ids": id_arr,
-                                "fill_val": fv,
-                                "units": units,
-                            }
-                        else:
-                            varset[var_name]["values"] = np.concatenate(
-                                (
-                                    varset[var_name]["values"],
-                                    variable,
-                                )
-                            )
-                            varset[var_name]["times"] = np.concatenate(
-                                (
-                                    varset[var_name]["times"],
-                                    time_arr,
-                                )
-                            )
-                            varset[var_name]["phenom_ids"] = np.concatenate(
-                                (
-                                    varset[var_name]["phenom_ids"],
-                                    id_arr,
-                                )
-                            )
+                        #     varset[var_name] = {
+                        #         "values": variable,
+                        #         "times": time_arr,
+                        #         "phenom_ids": id_arr,
+                        #         "fill_val": fv,
+                        #         "units": units,
+                        #     }
+                        # else:
+                        #     varset[var_name]["values"] = np.concatenate(
+                        #         (
+                        #             varset[var_name]["values"],
+                        #             variable,
+                        #         )
+                        #     )
+                        #     varset[var_name]["times"] = np.concatenate(
+                        #         (
+                        #             varset[var_name]["times"],
+                        #             time_arr,
+                        #         )
+                        #     )
+                        #     varset[var_name]["phenom_ids"] = np.concatenate(
+                        #         (
+                        #             varset[var_name]["phenom_ids"],
+                        #             id_arr,
+                        #         )
+                        #     )
 
                         # collect stats info
                         try:
@@ -141,48 +141,59 @@ def get_plot_data(
 
     # collapse all the file contents into a single dict for ease
     varset = OrderedDict()
+    time_arr = []
+    anom_id_arr = []
     for dataset in datasets:
         (vset, sset) = dataset
-        for varname in vset:
-            varset[varname] = vset[varname]
+        # for varname in vset:
+            # varset[varname] = vset[varname]
+        for varname in sset:
             varset[varname]["stats"] = sset[varname]
+            var_times = list(sset[varname].keys())
+            time_arr.extend(var_times)
+            for var_time in var_times:
+                anom_id_arr.extend(list(sset[varname][var_time].keys()))
+
+    time_arr = sorted(list(set(time_arr)))
+    anom_id_arr = sorted(list(set(anom_id_arr)))
 
     # stack all the variable values together into a single nD array
     plotset = {}
     for var_name in varset:
-        vals = varset[var_name]["values"][:, 2]  # grab the value column
+        # vals = varset[var_name]["values"][:, 2]  # grab the value column
         if "values" not in plotset:
             plotset["title"] = to_title(var_name)
-            plotset["values"] = vals
+            # plotset["values"] = vals
+            plotset["values"] = []
             plotset["axis_labels"] = [f'{var_name} ({varset[var_name]["units"]})']
             plotset["var_list"] = [var_name]
         else:
             plotset["title"] = f'{plotset["title"]} x {to_title(var_name)}'
-            plotset["values"] = np.c_[plotset["values"], vals]
+            # plotset["values"] = np.c_[plotset["values"], vals]
             plotset["axis_labels"].append(f'{var_name} ({varset[var_name]["units"]})')
             plotset["var_list"].append(var_name)
 
     # stack in the lat, lon, time, and phenom ids and pull fill value
     fill_value = None
-    for var_name in varset:
+    # for var_name in varset:
         # grab the first two columns data format is [lat, long, value]
-        lat_lon = varset[var_name]["values"][:, :2]
-        plotset["values"] = np.c_[
-            plotset["values"],
-            lat_lon,
-            varset[var_name]["times"],
-            varset[var_name]["phenom_ids"],
-        ]
+        # lat_lon = varset[var_name]["values"][:, :2]
+        # plotset["values"] = np.c_[
+        #     plotset["values"],
+        #     lat_lon,
+        #     varset[var_name]["times"],
+        #     varset[var_name]["phenom_ids"],
+        # ]
 
         # grab the fill value
-        fill_value = varset[var_name]["fill_val"]
+        # fill_value = varset[var_name]["fill_val"]
 
         # all the lats, lons, times, IDs, and fill values should match so we just need to do this once
-        break
+        # break
 
     # stack the stats together into wide table
-    time_arr = sorted(list(set(plotset["values"][:, -2])))
-    anom_id_arr = sorted(list(set(plotset["values"][:, -1])))
+    # time_arr = sorted(list(set(plotset["values"][:, -2])))
+    # anom_id_arr = sorted(list(set(plotset["values"][:, -1])))
 
     # build list of column names
     stats_columns = ["datetime", "anom_id"]
@@ -194,7 +205,6 @@ def get_plot_data(
             f"{var_name}_std_dev",
         ]
 
-    # TODO - account for missing time steps
     # build rows of stats data
     stats_rows = []
     for time in time_arr:
@@ -219,7 +229,7 @@ def get_plot_data(
 
     # Mask out plot values
     # init a mask for removing rows by removing None values
-    mask = plotset["values"] != None
+    # mask = plotset["values"] != None
     stats_mask = None
 
     # optionally remove rows that contain the fill value
@@ -228,14 +238,14 @@ def get_plot_data(
         expected_fill = -9999.0
 
         # add the fill values to mask
-        mask = np.logical_and(
-            mask,
-            ~(
-                np.logical_or(
-                    plotset["values"] == fill_value, plotset["values"] == expected_fill
-                )
-            ),
-        )
+        # mask = np.logical_and(
+        #     mask,
+        #     ~(
+        #         np.logical_or(
+        #             plotset["values"] == fill_value, plotset["values"] == expected_fill
+        #         )
+        #     ),
+        # )
 
         # remove fill from stats
         plotset["stats"]["rows"] = (
@@ -243,40 +253,40 @@ def get_plot_data(
         )
 
     # index from the right because the number of value columns is variable
-    anom_ind = -1
-    lon_ind = -3
-    lat_ind = -4
+    # anom_ind = -1
+    # lon_ind = -3
+    # lat_ind = -4
 
     # area is inclusive within [min_lon(x), min_lat(y), max_lon(x), max_lat(y)]
     # includes all points for all anomalies that intersect the bounds
-    if area is not None:
-        if isinstance(area, list) and len(area) == 4:
-            tmp_mask = mask.copy()
+    # if area is not None:
+    #     if isinstance(area, list) and len(area) == 4:
+            # tmp_mask = mask.copy()
 
-            # mask to only values within the specified bounds
-            tmp_mask[:, lon_ind] = np.ma.masked_inside(
-                plotset["values"][:, lon_ind], area[0], area[2]
-            ).mask
-            tmp_mask[:, lat_ind] = np.ma.masked_inside(
-                plotset["values"][:, lat_ind], area[1], area[3]
-            ).mask
+            # # mask to only values within the specified bounds
+            # tmp_mask[:, lon_ind] = np.ma.masked_inside(
+            #     plotset["values"][:, lon_ind], area[0], area[2]
+            # ).mask
+            # tmp_mask[:, lat_ind] = np.ma.masked_inside(
+            #     plotset["values"][:, lat_ind], area[1], area[3]
+            # ).mask
 
             # find the set of unique anomaly IDs within those bounds
             # add them to set manually specified in the request
-            filtered_values = plotset["values"][np.all(tmp_mask, axis=1), :]
-            filtered_anom_ids = np.unique(filtered_values[:, anom_ind])
-            anomaly_ids = np.concatenate((anomaly_ids, filtered_anom_ids))
-        else:
-            # no way to correct poor formatting
-            logging.warning(f"Improper bounding box format: {area}")
+            # filtered_values = plotset["values"][np.all(tmp_mask, axis=1), :]
+            # filtered_anom_ids = np.unique(filtered_values[:, anom_ind])
+            # anomaly_ids = np.concatenate((anomaly_ids, filtered_anom_ids))
+        # else:
+            # # no way to correct poor formatting
+            # logging.warning(f"Improper bounding box format: {area}")
 
     # anomaly ids is a list of anomalies to include
     if len(anomaly_ids) > 0:
-        mask[:, anom_ind] = np.isin(plotset["values"][:, anom_ind], anomaly_ids)
+        # mask[:, anom_ind] = np.isin(plotset["values"][:, anom_ind], anomaly_ids)
         stats_mask = plotset["stats"]["rows"]["anom_id"].isin(anomaly_ids)
 
     # apply mask to values
-    plotset["values"] = plotset["values"][np.all(mask, axis=1), :]
+    # plotset["values"] = plotset["values"][np.all(mask, axis=1), :]
     if stats_mask is not None:
         plotset["stats"]["rows"] = plotset["stats"]["rows"][stats_mask]
 
@@ -291,7 +301,7 @@ def dump_plot_data(plotData):
 
     # convert values to a Pandas DataFrame because json serializing is so much faster
     # TODO - convert to DataFrames during data aggregation for better filtering performance
-    plot_vals = pd.DataFrame(plotData["values"])
+    # plot_vals = pd.DataFrame(plotData["values"])
     stats_vals = plotData["stats"]["rows"]
     stats_headers = plotData["stats"]["columns"]
 
@@ -303,7 +313,8 @@ def dump_plot_data(plotData):
     plot_data_str = json.dumps(plotData)
 
     # get data array as string
-    vals_str = plot_vals.to_json(orient="values")
+    # vals_str = plot_vals.to_json(orient="values")
+    vals_str = '[]'
     stats_vals_str = stats_vals.to_json(orient="values")
 
     stats_sub_str = (
